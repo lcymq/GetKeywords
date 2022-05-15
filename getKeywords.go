a0381d01f628e8ddbf19e37bab1b90b862fe68ab
Cloris <cloris.li@pocketsocial.com>
2022-05-15 03:24:44 +0000

    add get keywords main files

---
+++ getKeywords.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	root := "."
+	filepath.Walk(root, walkfunc)
+}
+
+func walkfunc(path string, info os.FileInfo, err error) error {
+
+	//过滤目录
+	if info.IsDir() {
+		return nil
+	}
+	// 打印文件名
+	// fmt.Println(filepath.Base(path))
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+
+	// 每行读取
+    line := 1
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		// 抓取fmt所在的行
+		if strings.Contains(scanner.Text(), "fmt") {
+			// fmt.Println(scanner.Text()), // 这里就可以当成字符串处理该行
+            fmt.Println(f.Name(), ":", line)
+        }
+        line++
+	}
+
+	err = scanner.Err()
+	fmt.Println(err)
+
+	return nil
+}
